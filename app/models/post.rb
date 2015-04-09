a137ddbb8ac29ed5ab9ec9f0c9b170dfb52b10de
Garth Gaughan <garth0323@gmail.com>
2015-04-09 16:39:17 -0700

    try and find view

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :page
 
   def self.upload_from_twitter(i, page)
-    binding.pry
     Post.create(
       profile_image_url: i["profile_image_url"],
       screen_name: i["screen_name"],
